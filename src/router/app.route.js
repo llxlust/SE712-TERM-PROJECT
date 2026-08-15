ce400b1ad49611c8fffffee04f480f77a9bde4de
Krittpas Pingfong <krittpaspingfong@MacBook-Air-khxng-Krittpas.local>
2026-08-15 14:05:12 +0700

    init

--- src/router/app.route.js
+++ src/router/app.route.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Database = require("../db/db");
 const { getAllProgram } = require("../db/repositories/program.repository");
+
 router.get("/program/get-all-program", async (req, res) => {
   try {
     const db = new Database();
@@ -23,6 +24,40 @@ router.get("/program/get-all-program", async (req, res) => {
       timestamp: Date.now(),
     });
   }
+});
+
+router.post(`/program/register-program`, async (req, res) => {
+  const {
+    first_name,
+    last_name,
+    phone,
+    age,
+    email,
+    national,
+    size,
+    program_id,
+  } = req.body;
+
+  if (
+    !first_name ||
+    !last_name ||
+    !phone ||
+    !age ||
+    !email ||
+    !national ||
+    !size ||
+    !program_id
+  ) {
+    return res
+      .status(400)
+      .json({ error: "Bad Request", success: false, timestamp: Date.now() });
+  }
+
+  console.log(req.body);
+
+  res
+    .status(201)
+    .json({ data: "Successful Register,success:true,timestamp:Date.now()" });
 });
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const AppRouter = require("./router/app.route");
 const User = require("./db/db");
-
+const path = require("path");
 const app = express();
 const PORT = 3000;
 
@@ -10,5 +10,7 @@ app.use(express.static("public"));
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(`/api/v1/`, AppRouter);
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
